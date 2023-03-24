04925c4ab608d8bd2ab4680600b19ec228c77082
Anmol K Sachan <anmol221999@gmail.com>
2023-03-24 10:27:12 +0530

    Delete p directory

--- p/script.js
+++
@@ -1,7 +0,0 @@
-// Load the README.md file and insert it into the page
-fetch('README.md')
-  .then(response => response.text())
-  .then(text => {
-    const readmeContent = document.getElementById('readme-content');
-    readmeContent.innerHTML = text;
-  });
